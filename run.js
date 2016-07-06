fba6387c3395cf4e76cdfe8714c5d1e6b07381b8
Philippe Vienne <philippegeek@gmail.com>
2016-07-06 20:15:41 +0200

    Clean up code with clean callbacks

--- run.js
+++ run.js
@@ -15,43 +15,48 @@ var T = new Twit({
 
 var tweets = {};
 var count = 0;
-var twitter_callback = function(error, data, response){
-    data.statuses.forEach(function(e){
-        if(tweets[e.user.screen_name.toString()] == undefined){
-            tweets[e.user.screen_name.toString()] = 1;
+var twitter_callback = function(callback) {
+    return function(error, data, response){
+        data.statuses.forEach(function(e){
+            if(tweets[e.user.screen_name.toString()] == undefined){
+                tweets[e.user.screen_name.toString()] = 1;
+            } else {
+                tweets[e.user.screen_name.toString()]++;
+            }
+        });
+        count += data.statuses.length;
+        if(data.search_metadata.next_results != undefined){
+            T.get('search/tweets',querystring.parse(data.search_metadata.next_results.substring(1)), twitter_callback(callback));
         } else {
-            tweets[e.user.screen_name.toString()]++;
+            callback();
         }
-    });
-    count += data.statuses.length;
-    if(data.search_metadata.next_results != undefined){
-        T.get('search/tweets',querystring.parse(data.search_metadata.next_results.substring(1)), twitter_callback);
-    } else {
-        endedCallback();
     }
 };
 
 var date = (new Date());
 var today = dateFormat(date, "yyyy-mm-dd");
-T.get('search/tweets', { q: '#'+process.env.hashtag+' since:'+today+' -RT', count:100 }, twitter_callback);
 
-function endedCallback(){
-    var tuples = [];
+T.get('search/tweets', { q: '#'+process.env.hashtag+' since:'+today+' -RT', count:100 }, twitter_callback(function(){
 
-    for (var k in tweets) tuples.push([k, tweets[k]]);
+    // Make order
+    var scores = [];
 
-    tuples.sort(function(a, b) {
+    for (var k in tweets) scores.push([k, tweets[k]]);
+
+    scores.sort(function(a, b) {
         a = a[1];
         b = b[1];
 
         return a < b ? -1 : (a > b ? 1 : 0);
     });
 
-    for (var i = tuples.length-1; i > tuples.length-4 && i >= 0; i--) {
-        var key = tuples[i][0];
-        var value = tuples[i][1];
+    // Print results
+    for (var i = scores.length-1; i > scores.length-4 && i >= 0; i--) {
+        var key = scores[i][0];
+        var value = scores[i][1];
         console.log(key +' : '+value);
     }
 
     console.log(count + ' tweets today for #SophiaConf2016');
-}
+
+}));
